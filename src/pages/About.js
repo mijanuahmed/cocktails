6b278f2698dbcd959f55c69f9000b590a6c0f867
Mijan Uddin Ahmed <63743944+mijanuahmed@users.noreply.github.com>
2021-09-02 15:26:54 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit,
+        maxime sequi tempora libero exercitationem, at perferendis excepturi
+        quisquam quod accusantium blanditiis iusto tenetur tempore quis
+        laboriosam hic voluptatum est id et distinctio nobis. Provident maiores
+        obcaecati fuga molestiae repellat officiis!
+      </p>
+      <h1 className="section-title">about us</h1>
+    </section>
+  );
+};
+
+export default About;
